2780e6d3613671632e1931cafab8bf5e3141c005
neixir <neixir@gmail.com>
2025-08-01 21:22:58 +0200

    CH3 L4 Create a Queue

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,56 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"os/signal"
+
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/pubsub"
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/routing"
+	amqp "github.com/rabbitmq/amqp091-go"
+)
 
 func main() {
+	connectionString := "amqp://guest:guest@localhost:5672/"
+
 	fmt.Println("Starting Peril server...")
+
+	// Creating the connection
+	conn, err := amqp.Dial(connectionString)
+
+	if err != nil {
+		fmt.Println("Something happened creating the connection.")
+		return
+	}
+
+	defer conn.Close()
+
+	fmt.Println("Connection successful!")
+
+	// Creating the channel
+	channel, err := conn.Channel()
+	if err != nil {
+		fmt.Println("Something happened creating the channel.")
+		return
+	}
+
+	// Publishing something
+	data := routing.PlayingState{
+		IsPaused: true,
+	}
+
+	pubsub.PublishJSON(channel, routing.ExchangePerilDirect, routing.PauseKey, data)
+
+	// Wait for a signal (e.g. Ctrl+C) to exit the program.
+	// If a signal is received, print a message to the console that the program is shutting down and close the connection.
+
+	// The Go standard library has a package called os/signal that you can use to listen for signals.
+	// Here's an example of how you can listen for a signal:
+	// wait for ctrl+c
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, os.Interrupt)
+	<-signalChan
+
+	fmt.Println("Shutting down...")
+
 }
