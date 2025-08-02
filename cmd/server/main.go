3c3792a06f2a0a7de61614c6bca97c89aa739d5d
neixir <neixir@gmail.com>
2025-08-02 12:17:15 +0200

    CH3 L06 Decoupling

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,9 +2,8 @@ package main
 
 import (
 	"fmt"
-	"os"
-	"os/signal"
 
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/gamelogic"
 	"github.com/bootdotdev/learn-pub-sub-starter/internal/pubsub"
 	"github.com/bootdotdev/learn-pub-sub-starter/internal/routing"
 	amqp "github.com/rabbitmq/amqp091-go"
@@ -33,12 +32,37 @@ func main() {
 		return
 	}
 
-	// Publishing something
-	data := routing.PlayingState{
-		IsPaused: true,
-	}
+	data := routing.PlayingState{}
 
-	pubsub.PublishJSON(channel, routing.ExchangePerilDirect, routing.PauseKey, data)
+	gamelogic.PrintServerHelp()
+	quitGame := false
+	for !quitGame {
+		input := gamelogic.GetInput()
+		if len(input) == 0 {
+			continue
+		}
+
+		switch input[0] {
+		case "pause":
+			// log to the console that you're sending a pause message, and publish the pause message as you were doing before.
+			fmt.Println("Pausing game.")
+			data.IsPaused = true
+			pubsub.PublishJSON(channel, routing.ExchangePerilDirect, routing.PauseKey, data)
+		case "resume":
+			// log to the console that you're sending a resume message, and publish the resume message as you were doing before.
+			fmt.Println("Resuming game.")
+			data.IsPaused = false
+			pubsub.PublishJSON(channel, routing.ExchangePerilDirect, routing.PauseKey, data)
+		case "quit":
+			// log to the console that you're exiting, and break out of the loop.
+			fmt.Println("Quitting game.")
+			quitGame = true
+		default:
+			// If it's anything else, log to the console that you don't understand the command.
+			fmt.Println("Unknown command.")
+
+		}
+	}
 
 	// Wait for a signal (e.g. Ctrl+C) to exit the program.
 	// If a signal is received, print a message to the console that the program is shutting down and close the connection.
@@ -46,10 +70,12 @@ func main() {
 	// The Go standard library has a package called os/signal that you can use to listen for signals.
 	// Here's an example of how you can listen for a signal:
 	// wait for ctrl+c
-	signalChan := make(chan os.Signal, 1)
-	signal.Notify(signalChan, os.Interrupt)
-	<-signalChan
+	/*
+		signalChan := make(chan os.Signal, 1)
+		signal.Notify(signalChan, os.Interrupt)
+		<-signalChan
 
-	fmt.Println("Shutting down...")
+		fmt.Println("Shutting down...")
+	*/
 
 }
